c1eb6a5b4168f694f58ce810f28f58303f23b11f
VaishnaviReddy21 <123863381+VaishnaviReddy21@users.noreply.github.com>
2026-01-04 12:05:17 +0530

    Add files via upload

--- app.routes.ts
+++ app.routes.ts
@@ -1,3 +1,30 @@
-import { Routes } from '@angular/router';
+/*import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [];*/
+
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { authGuard } from './guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/login'
+  }
+];
+
--- app.ts
+++ app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('user-portal');
+  protected readonly title = signal('eDLogin');
 }
